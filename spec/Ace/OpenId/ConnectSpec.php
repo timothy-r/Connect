ee56ff57f9bb8c8030cd81c80feca39516aaea43
Tim Rodger <tim.rodger@gmail.com>
2014-10-18 12:50:26 +0100

    Adds Nonce token type and Connect test

--- spec/Ace/OpenId/ConnectSpec.php
+++ spec/Ace/OpenId/ConnectSpec.php
@@ -16,4 +16,9 @@ class ConnectSpec extends ObjectBehavior
     {
         $this->generateCsrfToken()->shouldHaveType('Ace\Token\Csrf');
     }
+
+    public function it_generates_nonce_token()
+    {
+        $this->generateNonceToken()->shouldHaveType('Ace\Token\Nonce');
+    }
 }
--- src/Ace/OpenId/Connect.php
+++ src/Ace/OpenId/Connect.php
@@ -1,11 +1,17 @@
 <?php namespace Ace\OpenId;
 
 use Ace\Token\Csrf;
+use Ace\Token\Nonce;
 
 class Connect
 {
     public function generateCsrfToken()
     {
         return new Csrf;
+    }
+
+    public function generateNonceToken()
+    {
+        return new Nonce;
     }
 }
